50844ed6ad8677a3f6d9b5f0966b0839da3cc9bb
marasolarp <mariasolarp@gmail.com>
2018-01-31 20:30:09 -0300

    css a jv en carusel numbertext

--- assets/js/carusel-numtext.js
+++ assets/js/carusel-numtext.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-	
-var pos = 0;
-carouselNum();
 
-function carouselNum() {
+	
+	$('.img-carousel').css('position', 'absolute');	
+	$('.text').css({'background' : 'rgba(0, 8, 20, 0.5)', 'position' : 'relative', 'bottom' : '6em', 'text-align' : 'center', 'padding' : '1.3em', 'color' : '#fff', 'font-size' : '1.5em'});
+	
+	
+	
+	
+});
+var pos = 0;
+	carouselNum();
+
+	function carouselNum() {
        var carouselText = document.getElementsByClassName("carousel");
        for ( var i = 0; i < carouselText.length; i++) {
         carouselText[i].style.display = "none";
@@ -12,10 +20,7 @@ function carouselNum() {
        if(pos > carouselText.length) {pos = 1}
        carouselText[pos-1].style.display = "block";
        setTimeout(carouselNum,4000);       
-}
-
-
-});
+	}
 
 
 
@@ -24,4 +29,19 @@ function carouselNum() {
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
