6f786b3de4319392287bc6b683c9c529134e4b58
marasolarp <mariasolarp@gmail.com>
2018-01-31 21:45:36 -0300

    agregue css en la funcion mas cambios en la funcion del carusel numtetx

--- assets/js/carusel-numtext.js
+++ assets/js/carusel-numtext.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
 
 	
-	$('.img-carousel').css('position', 'absolute');	
-	$('.text').css({'background' : 'rgba(0, 8, 20, 0.5)', 'position' : 'relative', 'bottom' : '6em', 'text-align' : 'center', 'padding' : '1.3em', 'color' : '#fff', 'font-size' : '1.5em'});
-	
-	
-	
-	
-});
 var pos = 0;
-	carouselNum();
+    carouselNum();
 
-	function carouselNum() {
+    function carouselNum() {
        var carouselText = document.getElementsByClassName("carousel");
        for ( var i = 0; i < carouselText.length; i++) {
-        carouselText[i].style.display = "none";
+        $(carouselText[i]).hide();
        }
        pos++;
        if(pos > carouselText.length) {pos = 1}
-       carouselText[pos-1].style.display = "block";
-       setTimeout(carouselNum,4000);       
-	}
-
+       $(carouselText[pos-1]).show();
+           $('.carousel').css('position', 'absolute');
+           $('.text').css({'background' : 'rgba(0, 8, 20, 0.5)', 'position' : 'absolute', 'width': '100%', 'bottom' : '0', 'text-align' : 'center', 'padding' : '1.3em 0 1.3em 0', 'color' : '#fff', 'font-size' : '1.5em'});
+       setTimeout(carouselNum,4000);
+   }
+});   
 
 
 
