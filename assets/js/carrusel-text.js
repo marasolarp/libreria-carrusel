f04bd487e7137d390f2ea5122ff509a12d0541eb
marasolarp <mariasolarp@gmail.com>
2018-02-02 02:43:18 -0300

    agregando instrucciones de uso al html del demo

--- assets/js/carrusel-text.js
+++ assets/js/carrusel-text.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
 	
   var pos = 0;
-  carouselNum();
+  carouselText3();
 
-    function carouselNum() {
+    function carouselText3() {
        let carouselText = $('.carousel');
        for ( var i = 0; i < carouselText.length; i++) {
         $(carouselText[i]).hide();
@@ -12,21 +12,13 @@ $(document).ready(function() {
        pos++;
        if(pos > carouselText.length) {pos = 1}
        $(carouselText[pos-1]).show();
-        $('caruselV3').css('width' : '100%', 'position' : 'relative' )
-        $('.carouseV3 .carousel .img-carusel').css({'width', '100%'});
-        $('.carusel .text').css({'position' : 'absolute', 'width': '100%', 'height' : '100%','top' : '0', 'left' : '0', 'display: flex', 'flex-direction' : 'column', 'justify-content' : 'center', 'align-items' : 'center', 'text-align' : 'center', 'padding' : '5px 40px', 'color' : '#fff', 'z-index' : '1', 'font-size' : '20px', 'margin-top' :'10px'});      
-        setTimeout(carouselNum,4000);  
+        $('caruselV3').css({'width' : '100%', 'position' : 'relative' });
+        $('.carouseV3, .carousel, .img-carousel').css({'width' : '100%'});
+        $('.carusel, .text').css({'position' : 'absolute', 'width': '100%', 'height' : '100%', 'top' : '0', 'left' : '0', 'display' : 'flex', 'flex-direction' : 'column', 'justify-content' : 'center', 'align-items' : 'center', 'text-align' : 'center', 'padding' : '5px 40px', 'color' : '#fff', 'z-index' : '1', 'font-size' : '20px', 'margin-top' :'10px'});      
+        setTimeout(carouselText3,3000);  
       }
 });
 
-
-/*$('.img-carousel').css({'position' : 'relative', 'text-align' : 'center', 'color' : '#fff'});
-          $('.botom-left').css({'position' : 'absolute', 'bottom' : '8px', 'left' : '16px'});
-
-
-        $('.img').css('position', 'relative');
-        
-       setTimeout(carouselNum,4000);*/
 
 
 
